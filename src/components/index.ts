179e219a70f0f04dda8f2117e34a7680653c4630
nohyewon <yw4913@naver.com>
2025-07-30 23:21:46 +0900

    :sparkles:[feat]: 통화 메인 화면 UI 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,12 @@
+// Common
+export * from './common/index';
+
 // Init
 export { default as RoleSelector } from './RoleSelector';
 export { default as CheckItem } from './CheckItem';
 export { default as RoleCard } from './RoleCard';
 export { default as GenderButton } from './GenderButton';
+
+// Call
+export { default as CallBtn } from './CallBtn';
+export { default as RecordCard } from './RecordCard';
